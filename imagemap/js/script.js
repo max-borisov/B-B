c2fab3ad74b938dadd764a5f19d5e756e55e0e6e
max-borisov <max.borisov@yahoo.com>
2014-02-25 11:56:10 +0200

    updates

--- imagemap/js/script.js
+++ imagemap/js/script.js
@@ -1,4 +1,12 @@
 $(function() {
     // Init imagemap
     MapImageIcon.init();
+
+    // Show icons with tooltip
+    MapImageIcon.showIconSet(iconSet);
 })
+var iconSet = [
+    {x: 271, y: 109, text: 'The PHP development team announces the immediate availability of PHP 5.6.0alpha2'},
+    {x: 336, y: 304, text: 'Output encoding handling'},
+    {x: 530, y: 287, text: 'All users of PHP are encouraged to test this version'}
+];
