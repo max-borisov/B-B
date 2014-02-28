bf08f2b608d2d729e7401fe489d16fb38da201f7
max-borisov <max.borisov@yahoo.com>
2014-02-28 15:58:27 +0200

    share btw

---
+++ social/share.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>B&B social sharing</title>
+</head>
+<body>
+
+<?php
+$name = 'ButchersAndBicycles';
+$url = 'http://www.butchersandbicycles.com';
+$media = 'http://www.butchersandbicycles.com/images/about-img.jpg';
+?>
+<ul>
+    <li><a target="_blank" href="https://twitter.com/home?status=<?=$name;?>+<?=$url;?>">TW</a></li>
+    <li><a target="_blank" href="http://www.facebook.com/sharer.php?u=<?=$url;?>">FB</a></li>
+    <li><a target="_blank" href="https://plus.google.com/share?url=<?=$url;?>">G+</a></li>
+    <li><a target="_blank" href="http://www.stumbleupon.com/submit?url=<?=$url;?>&title=<?=$name;?>">STUP</a></li>
+    <li><a href="//www.pinterest.com/pin/create/button/?url=<?=$url;?>&media=<?=$media;?>&description=<?=$name;?>" data-pin-do="buttonPin" data-pin-config="none"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_20.png" /></a></li>
+</ul>
+
+
+<!--<a href="http://www.pinterest.com/pin/create/button/
+        ?url=http%3A%2F%2Fwww.flickr.com%2Fphotos%2Fkentbrew%2F6851755809%2F
+        &media=http%3A%2F%2Ffarm8.staticflickr.com%2F7027%2F6851755809_df5b2051c9_z.jpg
+        &description=Next%20stop%3A%20Pinterest"
+   data-pin-do="buttonPin"
+   data-pin-config="above">
+    <img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" />
+</a>-->
+
+
+<!--<a href="//www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.flickr.com%2Fphotos%2Fkentbrew%2F6851755809%2F&media=http%3A%2F%2Ffarm8.staticflickr.com%2F7027%2F6851755809_df5b2051c9_z.jpg&description=Next%20stop%3A%20Pinterest" data-pin-do="buttonPin" data-pin-config="none"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_20.png" /></a>
+<!-- Please call pinit.js only once per page -->
+<!--<script type="text/javascript" async src="//assets.pinterest.com/js/pinit.js"></script>-->
+
+<!--<a href="//www.pinterest.com/pin/create/button/?url=<?/*=$url;*/?>&media=<?/*=$media;*/?>&description=<?/*=$name;*/?>" data-pin-do="buttonPin" data-pin-config="none"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_20.png" /></a>-->
+<!-- Please call pinit.js only once per page -->
+<script type="text/javascript" async src="//assets.pinterest.com/js/pinit.js"></script>
+
+</body>
+</html>
